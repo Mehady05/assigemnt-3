04067e1274e02c3a2ad74d8cf3168177f1a5fe54
Mehady05 <48560850+Mehady05@users.noreply.github.com>
2021-05-29 21:55:30 +0600

    assignmnet

---
+++ assignment.js
@@ -0,0 +1,76 @@
+// 01
+
+// function kilometerToMeter(kilo) {
+//   var convert = kilo * 1000;
+//   return convert;
+// }
+
+// var result = kilometerToMeter(2);
+// console.log(result);
+
+// 02.
+// function budgetCalculator(clock, phone, laptop){
+//     var clock = clock * 50;
+//     var phone = phone * 100;
+//     var laptop = laptop * 500;
+//     var total = clock + phone + laptop;
+//     return total;
+// }
+
+// var all = budgetCalculator(1, 1, 1);
+// console.log(all);
+
+// 03.
+
+// function hotelCost(day) {
+//   var cost = 0;
+
+//   if (day <= 10) {
+//     cost = day * 100;
+//   } else if (day <= 20) {
+//     var first10 = 10 * 100;
+//     var remainDay = day - 10;
+//     var second10 = remainDay * 80;
+//     cost = first10 + second10;
+//   } else {
+//     var first10 = 10 * 100;
+//     var second10 = 10 * 80;
+//     var remainDay = day - 20;
+//     var lastDay = remainDay * 50;
+//     cost = first10 + second10 + lastDay;
+//   }
+//   return cost;
+// }
+
+// var result = hotelCost (2);
+// console.log(result);
+
+// 04.
+
+
+// function getLargeString(arr) {
+//   var largeString = "";
+//   arr.forEach(function (element) {
+//     if (largeString == "") largeString = element;
+//     if (largeString.length < element.length) largeString = element;
+//   });
+//   console.log(largeString);
+// }
+
+
+// var megaFriend = [
+//     "mehedi",
+//     "naiem",
+//     "niamul",
+//     "piranto",
+//     "jafor",
+//     "shipon",
+//     "kuddusAli",
+//   ];
+  
+// getLargeString(megaFriend);
+
+
+
+
+
